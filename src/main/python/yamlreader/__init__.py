5fe91fa78e53d2bcf1b4acefbbbf86a1135d73b9
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2015-11-02 19:12:49 +0100

    Reintroduce __version__, this time at the module level

--- src/main/python/yamlreader/__init__.py
+++ src/main/python/yamlreader/__init__.py
@@ -4,4 +4,5 @@ from __future__ import print_function, absolute_import, unicode_literals, divisi
 
 from .yamlreader import data_merge, yaml_load, YamlReaderError
 
+__version__ = '3.0.2'
 __all__ = ['data_merge', 'yaml_load', 'YamlReaderError']
--- src/main/python/yamlreader/yamlreader.py
+++ src/main/python/yamlreader/yamlreader.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, absolute_import, unicode_literals, division
 
+from . import __version__
 from yaml import MarkedYAMLError, safe_load, safe_dump
 import glob
 import os
